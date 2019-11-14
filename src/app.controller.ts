dfabe4967114b167a02484d44e3388871f43880f
Kerry Ritter <kerrylritter@gmail.com>
2019-11-14 16:04:00 -0600

    master
    adding a check for transformers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,30 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RestController } from '../../nest-rest-framework/src';
+
+interface TodoItemData {
+  id: string;
+  description: string;
+  complete: string;
+}
+interface TodoItemDto {
+  id: string;
+  description: string;
+  complete: string;
+}
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+export class AppController extends RestController<
+  string,
+  TodoItemData,
+  TodoItemDto,
+  TodoItemDto
+> {
+  constructor(private readonly appService: AppService) {
+    super({
+      viewset: null,
+    })
+  }
 
   @Get()
   getHello(): string {
